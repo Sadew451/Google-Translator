ff685e050e6263fb47edd6ebb228311b8d63a4d2
sᴀᴅᴇᴡ ᴊᴀʏᴀsᴇᴋᴀʀᴀ <84616471+Sadew451@users.noreply.github.com>
2021-11-01 19:08:04 +0530

    Update translate.py

--- plugins/translate.py
+++ plugins/translate.py
@@ -11,10 +11,9 @@ from helper.langlist import langlist
 @Client.on_message(filters.private & filters.command(['start']))
 async def start(client, message):
           insert(int(message.chat.id))
-          await message.reply_text(text =f"Hello **{message.from_user.first_name }** \n\nI am simple Google Translater Bot \nI can translate any language to you selected language__\n[SDBOTs](https://telegra.ph/file/8ec3ccd54a4a6016986c4.jpg)",
+          await message.reply_text(text =f"👋 Hey  **{message.from_user.first_name }** \n\nI am simple Google Translater Bot \nI can translate any language to you selected language__\n[SDBOTs](https://telegra.ph/file/8ec3ccd54a4a6016986c4.jpg)",
 				   reply_to_message_id = message.message_id , parse_mode="markdown",
 				   reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("SDBOTs News 🙋‍♀️", url="https://t.me/SDBOTs_Inifinity")],
-					                              [InlineKeyboardButton("Bot Support 👥", url="https://t.me/SDBOTz")
 								      [InlineKeyboardButton("Bot Support 👥", url="https://t.me/SDBOTz"),
 								      InlineKeyboardButton("Devoloper 👩‍💻",url = "https://t.me/Itz_Sadew")]]))
             

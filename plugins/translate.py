3e54b3462cccff01d1e5e9cb9dde9fa652f48083
sᴀᴅᴇᴡ ᴊᴀʏᴀsᴇᴋᴀʀᴀ <84616471+Sadew451@users.noreply.github.com>
2021-11-01 18:46:23 +0530

    Update translate.py

--- plugins/translate.py
+++ plugins/translate.py
@@ -11,11 +11,11 @@ from helper.langlist import langlist
 @Client.on_message(filters.private & filters.command(['start']))
 async def start(client, message):
           insert(int(message.chat.id))
-          await message.reply_text(text =f"Hello **{message.from_user.first_name }** \n\nI am simple Google Translater Bot \nI can translate any language to you selected language__[SDBOTs](https://telegra.ph/file/a84e9304e2b99b18c4c48.jpg)",
+          await message.reply_text(text =f"Hello **{message.from_user.first_name }** \n\nI am simple Google Translater Bot \nI can translate any language to you selected language__[SDBOTs](https://telegra.ph/file/8ec3ccd54a4a6016986c4.jpg)",
 				   reply_to_message_id = message.message_id , parse_mode="markdown", 
-				   reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ],                 
-								      [InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/LNtechnical"),
-								       InlineKeyboardButton("How To Use",url = "https://youtu.be/dUYvenXiYKE")]]))
+				   reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('SDBOts News 🙋‍♀️" ,url="https://t.me/lntechnical") ],                 
+								      [InlineKeyboardButton("Bot Support 👥", url="https://t.me/SDBOTz"),
+								       InlineKeyboardButton("Devoloper 👩‍💻",url = "https://t.me/Itz_Sadew")]]))
             
             
 @Client.on_message(filters.private & filters.text  )

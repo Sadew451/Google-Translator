bd705fce4b1ad4606fa051234928a35f4bf9d9ae
sᴀᴅᴇᴡ ᴊᴀʏᴀsᴇᴋᴀʀᴀ <84616471+Sadew451@users.noreply.github.com>
2021-11-01 18:52:43 +0530

    Update translate.py

--- plugins/translate.py
+++ plugins/translate.py
@@ -13,8 +13,7 @@ async def start(client, message):
           insert(int(message.chat.id))
           await message.reply_text(text =f"Hello **{message.from_user.first_name }** \n\nI am simple Google Translater Bot \nI can translate any language to you selected language__,
 				   reply_to_message_id = message.message_id , parse_mode="markdown",
-				   reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('SDBOts News 🙋‍♀️" ,url="https://t.me/lntechnical")],
-								      [InlineKeyboardButton("Bot Support 👥", url="https://t.me/SDBOTz"),
+				   reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Bot Support 👥", url="https://t.me/SDBOTz"),
 								       InlineKeyboardButton("Devoloper 👩‍💻",url = "https://t.me/Itz_Sadew")]]))
             
             
